a520c729af771f2eb6c9284a2933c556bbab622c
Yoann Lehmann <yoann.lehmann1995@gmail.com>
2020-03-27 12:49:45 +0100

    Add SVG.js library. Create a testing SVG view, with mouse moving on rect element.

--- public/javascripts/eventlistener.js
+++ public/javascripts/eventlistener.js
@@ -1,5 +1,34 @@
 let btnNewShape = document.getElementById('new-shape');
 
+
+btnNewShape.addEventListener('mouseover', function(){
+    console.log("btn new shape mouse over");
+});
+
 btnNewShape.addEventListener('click', function(){
     console.log("btn new shape clicked !");
+    var draw = SVG().addTo('body').size(300, 300);
+    draw.attr('id', 'main');
+    var rect = draw.rect(100, 100).attr({ fill: '#f06' });
+    
+    rect.mouseover(function(){
+        console.log("Over the rect");
+        this.attr({fill: '#ddd'});
+    });
+
+    rect.on(['mousedown'], function(){
+        rect.on(['mousemove'], function(){
+            let bodyBoundingRect = document.body.getBoundingClientRect();
+            let drawBoundingRect = document.getElementById('main').getBoundingClientRect();
+    
+            let drawXPos = drawBoundingRect.left - bodyBoundingRect.left;
+            let drawYPos = drawBoundingRect.top - bodyBoundingRect.top;
+            
+            
+            this.attr({
+                x : event.clientX - (rect.width() / 2) - drawXPos,
+                y : event.clientY - (rect.height() / 2) - drawYPos
+            });
+        });
+    });
 })
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var SVGElementModel = require('../models/SVGElement.model');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 

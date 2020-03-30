a298a6b79e4e965e7ab7f1ba4ca8e00fd813ce7c
Yoann Lehmann <yoann.lehmann1995@gmail.com>
2020-03-30 15:14:32 +0200

    Add shape selection.

--- public/javascripts/Shape.model.js
+++ public/javascripts/Shape.model.js
@@ -21,6 +21,7 @@ class Shape
     OnMouseDownCallback(event)
     {
         console.log("MouseDown");
+        this.Select();
         this.mousePressed = true;
     }
 
@@ -45,58 +46,19 @@ class Shape
             });
         }
     }
-}
 
-/*
-class Shape 
-{
-    SVGShape = null;
-    mousePressed = false;
-    height = 0;
-    width = 0;
-    
-    construtor(container, height = 10, width = 10, xPos = 10, yPos = 10) {
-        
-        this.container  = container;
-        this.height     = height;
-        this.width      = width;
-        this.xPos       = xPos;
-        this.yPos       = yPos;
-
-        //this.shape = this.container.rect(100, 100).attr({ fill: '#f06' });
-        
-        /*
-
-        shape.addEventListener('mousedown', function(){
-            this.mousePressed = true;
-        });
-    
-        shape.addEventListener('mouseup', function(){
-            this.mousePressed = true;
-        });
-    
-        shape.addEventListener('mousemove', function(){
-            if(this.mousePressed)
-            {
-                let containerBoundingRect = container.getBoundingClientRect();
-        
-                let containerXPos = containerBoundingRect.left;
-                let containerYPos = containerBoundingRect.top;
-                
-                
-                this.attr({
-                    x : event.clientX - (this.width / 2) - containerXPos,
-                    y : event.clientY - (this.height / 2) - containerYPos
-                });
-            }
-        });
-        
+    OnClickCallback(event)
+    {
+        console.log("Element clicked.");
     }
 
-    print() {
-        console.log("Height " + this.height);
-        console.log(this.container);
-        console.log(this.shape);
+    Select()
+    {
+        this.SVGElement.css('opacity', '0.7');
+    }
+
+    Unselect()
+    {
+        this.SVGElement.css('opacity', '1');
     }
 }
-*/
--- public/javascripts/eventlistener.js
+++ public/javascripts/eventlistener.js
@@ -1,12 +1,19 @@
-let btnNewShape = document.getElementById('btn-new-shape');
+let btnNewSquare = document.getElementById('btn-new-square');
 let btnExport = document.getElementById('btn-export');
 let inputSvgFile = document.getElementById('input-svg-file');
 let mousePressed = false;
 let canvasContainer = document.getElementById('canvas-container');
 let mainCanvas = SVG().addTo('#canvas-container').size(1000, 800);
 mainCanvas.attr('id', 'main-canvas');
+let canvasBackground = mainCanvas.rect(1000,800).attr({fill:'#eee'});
+let listOfShape = [];
 
-inputSvgFile.addEventListener('click', function(){
+canvasBackground.on(['click'], function(event){
+    console.log("Canvas clicked.");
+    UnselectAllElement();
+});
+
+inputSvgFile.addEventListener('click', function(event){
     console.log("SVG File click");
 });
 
@@ -37,12 +44,24 @@ btnExport.addEventListener('click', function(){
     }
 });
 
+function UnselectAllElement()
+{
+    for(let i = 0; i < listOfShape.length; i++)
+    {
+        listOfShape[i].Unselect();
+    }
+}
+
 function bindShapeListener(shape)
 {
+    shape.SVGElement.on(['click'], function(event){
+        shape.OnClickCallback(event);
+    });
     shape.SVGElement.on(['mouseup'], function(event){
         shape.OnMouseUpCallback(event);
     });
     shape.SVGElement.on(['mousedown'], function(event){
+        UnselectAllElement();
         shape.OnMouseDownCallback(event);
     });
     shape.SVGElement.on(['mousemove'], function(event){
@@ -50,54 +69,14 @@ function bindShapeListener(shape)
     });
 }
 
-btnNewShape.addEventListener('click', function(){
-    console.log("btn new shape clicked !");
-    console.log()
+btnNewSquare.addEventListener('click', BtnAddNewSquareCallback);
 
-    //let shape = new Shape(mainCanvas, 50, 100, 100, 100, '#eee');
-    //shape.printDescription();
+function BtnAddNewSquareCallback()
+{
+    console.log("Add new square !");
 
     let square = new Square(mainCanvas, canvasContainer.getBoundingClientRect(), 150, 300, 300, "red");
     bindShapeListener(square);
-    let square2 = new Square(mainCanvas, canvasContainer.getBoundingClientRect(), 150, 150, 400, "red");
-    bindShapeListener(square2);
-    /*
-    square.shape.on(['mouseup'], function(event){
-        square.OnMouseUpCallback(event);
-    });
-    
-    square.shape.on(['mousedown'], function(event){
-        square.OnMouseDownCallback(event);
-    });
-    square.shape.on(['mousemove'], function(event){
-        square.OnMouseMoveCallback(event);
-    });
-    */
-    /*
-    rect = mainCanvas.rect(100, 100).attr({ fill: '#f06' });
-
-    rect.on(['mousedown'], function(){
-        mousePressed = true;
-    });
-
-    rect.on(['mouseup'], function(){
-        mousePressed = false;
-    });
-
-    rect.on(['mousemove'], function(){
-        if(mousePressed)
-        {
-            let drawBoundingRect = canvasContainer.getBoundingClientRect();
-    
-            let drawXPos = drawBoundingRect.left;
-            let drawYPos = drawBoundingRect.top;
-            
-            
-            this.attr({
-                x : event.clientX - (rect.width() / 2) - drawXPos,
-                y : event.clientY - (rect.height() / 2) - drawYPos
-            });
-        }
-    });
-    */
-})
+    listOfShape.push(square);
+    console.log(listOfShape);
+}

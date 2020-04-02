7d228fcdeaeda4c4d331fd99eb17ac0103a89de1
Yoann Lehmann <yoann.lehmann1995@gmail.com>
2020-04-02 16:21:17 +0200

    Debug of the move of the shapes in the canvas

--- public/javascripts/Shape.model.js
+++ public/javascripts/Shape.model.js
@@ -12,6 +12,7 @@ class Shape
         this.mousePressed = false;
         this.SVGElement = null;
         this.type = type;
+        this.oldMousePosition = null;
     }
 
     OnMouseOverCallback(event)
@@ -24,36 +25,53 @@ class Shape
     OnMouseDownCallback(event)
     {
         this.Select();
+
+        let containerXPos = this.containerBoundingRect.left;
+        let containerYPos = this.containerBoundingRect.top;
+
         this.mousePressed = true;
+        this.oldMousePosition = [
+            event.clientX - containerXPos,
+            event.clientY - containerYPos
+        ];
     }
 
     OnMouseUpCallback(event)
     {
         this.mousePressed = false;
+        this.oldMousePosition = null;
     }
 
     OnMouseMoveCallback(event)
     {
-        if(this.mousePressed)
+        if(this.mousePressed && this.oldMousePosition !== null) 
         {
             let containerXPos = this.containerBoundingRect.left;
             let containerYPos = this.containerBoundingRect.top;
             
-            let newPosX = event.clientX - (this.width / 2) - containerXPos;
-            let newPosY = event.clientY - (this.height / 2) - containerYPos;
+            let newMousePosition = [
+                event.clientX - containerXPos,
+                event.clientY - containerYPos
+            ];
 
-            (newPosX < 1 ? newPosX = 0 : newPosX = newPosX);
-            (newPosY < 1 ? newPosY = 0 : newPosY = newPosY);
+            let dx = newMousePosition[0] - this.oldMousePosition[0];
+            let dy = newMousePosition[1] - this.oldMousePosition[1];
+            let newPosX = this.SVGElement.x() + dx;//event.clientX  - containerXPos;
+            let newPosY = this.SVGElement.y() + dy;//event.clientY  - containerYPos;
+            this.oldMousePosition = newMousePosition;
+        
 
-            if(newPosX >= 0 && newPosX <= 1000 - this.width)
-            {
+            //if(newPosX >= 0 && newPosX <= 1000 - this.width)
+            //{
                 this.SVGElement.x(newPosX);
-            }
+                //this.SVGElement.move(this.SVGElement.x, 0);
+            //}
             
-            if(newPosY >= 0 && newPosY <= 800 - this.height)
-            {
+            //if(newPosY >= 0 && newPosY <= 800 - this.height)
+            //{
                 this.SVGElement.y(newPosY);
-            }
+                //this.SVGElement.move(0, dy);
+            //}
             
             
         }
